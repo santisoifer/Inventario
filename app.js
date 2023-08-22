15b80a99d038a86df934bf3ad365c883e1234371
Santi Soifer <santisoi2008@gmail.com>
2023-08-21 21:01:28 -0300

    Finished cookies and sessions with passport

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-require('dotenv').config()
+require("dotenv").config()
+const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
@@ -19,20 +20,78 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-// ? Cuando lo pase a db (IMPORTANTE: const userSchema = new mongoose.Schema({}), para que pueda agregar plugins):
-// mySchema.plugin(passportLocalMongoose);
+mongoose.connect("mongodb://127.0.0.1:27017/inventario");
 
-let UsersDB = [
-    { username: "admin", password: "supersecretPassw0rdadmin" }
-];
-
-app.get("/", (req, res) => {
-    res.render("signin");
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    products: Array
 });
 
-app.post("/signIn", (req, res) => {
-    const { username, password } = req.body;
-    let inputs = { username: username, password: password };
+userSchema.plugin(passportLocalMongoose);
+
+const User = mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.get("/", (req, res) => {
+    res.redirect("/inventario");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+app.get("/inventario", (req, res) => {
+    if (req.isAuthenticated()) {
+        res.render("index");
+    } else {
+        res.redirect("/login")
+    }
+});
+
+app.post("/login", (req, res) => {
+    const newUser = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    req.login(newUser, err => {
+        if (err) {
+            console.log(err);
+            res.redirect("/inventario");
+        } else {
+            passport.authenticate("local")(req, res, () => {
+                res.redirect("/inventario");
+            })
+        }
+    });
+
+});
+
+app.post("/register", (req, res) => {
+    User.register({username: req.body.username, products:[]}, req.body.password, (err, user) => {
+        if (!err) {
+            passport.authenticate("local")(req, res, () => {
+                res.redirect("/inventario");
+            });
+        } else {
+            console.log(err);
+            res.redirect("/register");
+        }
+    });
+});
+
+app.post("/logout", (req, res) => {
+    req.logOut();
+    res.redirect("/")
 });
 
 app.listen(3000, () => {
