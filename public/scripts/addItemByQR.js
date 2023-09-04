6c3d8f8ac02141b2858f32fddb795f895db37586
Santi Soifer <santisoi2008@gmail.com>
2023-09-04 20:10:11 -0300

    Finished actions when qr is scanned from the front

--- public/scripts/addItemByQR.js
+++ public/scripts/addItemByQR.js
@@ -1,4 +1,3 @@
-// TODO: Hacer que se completen los datos si se encontro el gtin. Sino, mostrar cartel de que hay que agregarlo
 function onScanSuccess(decodedText, decodedResult) {
     // Qué hacer si se detecta qr
     const dataToSend = {
@@ -19,16 +18,37 @@ function onScanSuccess(decodedText, decodedResult) {
             return response.json();
         })
         .then(data => {
+            const htmlElements = {
+                name: document.getElementById("productName"),
+                brand: document.getElementById("productBrand"),
+                divMessage: document.querySelector("#message"),
+                message: document.querySelector("#message p"),
+                gtinInput: document.querySelector("#productGTIN")
+            }
             if (data.foundedItem !== null) {
-                // Si se encontró un item
+                htmlElements.name.value = data.foundedItem.name;
+                htmlElements.brand.value = data.foundedItem.brand;
+
+                htmlElements.message.textContent = "Elemento encontrado!"
+                htmlElements.divMessage.style.display = "block";
+
+                setTimeout(() => {
+                    htmlElements.divMessage.style.display = "none";
+                }, 3500);
             } else {
                 // Si no se encontró ningun item (cartel y enviar gtin para agregar a db)
+                htmlElements.message.textContent = "Elemento no encontrado! Al agregarlo, se añadira a la base de datos."
+                htmlElements.divMessage.style.display = "block";
+                htmlElements.gtinInput.value = dataToSend.decodedText;
+
+                setTimeout(() => {
+                    htmlElements.divMessage.style.display = "none";
+                }, 3500);
             }
         })
         .catch(error => {
             console.error('Error:', error);
         });
-
 }
 
 const html5QrcodeScanner = new Html5QrcodeScanner(
