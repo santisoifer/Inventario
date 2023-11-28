0276025b1462179f8cc9cf3a9b3abaaf19d89c70
Santi Soifer <santisoi2008@gmail.com>
2023-11-28 12:22:46 -0300

    Made the dashboard page responsive

--- public/scripts/aside.js
+++ public/scripts/aside.js
@@ -5,6 +5,8 @@ const editItemLink = document.querySelector(".aside-panel__links__editItem");
 const makePurchasesLink = document.querySelector(".aside-panel__links__makePurchases");
 const purchasesArrivedLink = document.querySelector(".aside-panel__links__purchasesArrived");
 const logOutLink = document.querySelector(".aside-panel__links__logOut");
+const collapsableAside = document.getElementById("collapsable-aside");
+const asidePanel = document.getElementById("aside-panel");
 
 function addHoverClassToElement(element, className) {
     element.addEventListener('mouseover', () => {
@@ -15,3 +17,23 @@ function addHoverClassToElement(element, className) {
         element.classList.remove(className); // Elimina la clase cuando el mouse sale
     });
 }
+
+collapsableAside.style.display = "none";
+asidePanel.style.display = "block";
+
+function applyStyles() {
+    // Check the current screen width and apply styles accordingly
+    if (window.matchMedia('(max-width: 1520px)').matches) {
+        collapsableAside.style.display = "block";
+        asidePanel.style.display = "none";
+    } else {
+        asidePanel.style.display = "block";
+        collapsableAside.style.display = "none";
+    }
+}
+
+// Initial application of styles
+applyStyles();
+
+// Update styles on window resize
+window.addEventListener('resize', applyStyles);
